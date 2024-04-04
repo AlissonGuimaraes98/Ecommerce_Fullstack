7a78f219c4d09b0d011bc937013ec3d2a983f031
AlissonGuimaraes98 <alissonguimaraes478@gmail.com>
2024-04-03 23:15:42 -0300

    Inicio JS

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtém a referência para o elemento que exibe a quantidade
-    var quantidadeElemento = document.getElementById("quantidade");
 
-    // Obtém as referências para as imagens de plus e minus
+    //define a referencia de onde vaificar a quantidade e as imagens de menos e mais
+    var quantidadeElemento = document.getElementById("quantidade");
     var plusElemento = document.querySelector(".acoes .plus");
     var minusElemento = document.querySelector(".acoes .minus");
 
-    // Define um evento de clique para a imagem de plus
+    // define click nas imagens
     plusElemento.addEventListener("click", function() {
-        // Obtém o valor atual da quantidade
+        
         var quantidade = parseInt(quantidadeElemento.textContent);
-        // Incrementa a quantidade
+        
         quantidade++;
-        // Atualiza o texto do elemento com a nova quantidade
+        
         quantidadeElemento.textContent = quantidade;
     });
 
-    // Define um evento de clique para a imagem de minus
     minusElemento.addEventListener("click", function() {
-        // Obtém o valor atual da quantidade
+        
         var quantidade = parseInt(quantidadeElemento.textContent);
-        // Verifica se a quantidade é maior que zero para evitar valores negativos
+        
         if (quantidade > 0) {
-            // Decrementa a quantidade
+            
             quantidade--;
-            // Atualiza o texto do elemento com a nova quantidade
+            
             quantidadeElemento.textContent = quantidade;
         }
     });
